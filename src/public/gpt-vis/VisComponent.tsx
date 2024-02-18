c35d93cce9632f8fdcf0016fef1bb2452766d72f
Yiren Ding <dingx315@hotmail.com>
2024-02-17 23:41:42 -0700

    update img path in prod

--- src/public/gpt-vis/VisComponent.tsx
+++ src/public/gpt-vis/VisComponent.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 import {
   Container, Group, Image, Stack, Radio, Text,
 } from '@mantine/core';
@@ -8,11 +7,12 @@ function VisCompnent({ parameters }: { parameters: any }) {
   const {
     imgName, options, correctIndex, question,
   } = parameters;
+  const imgPathPrefix = import.meta.env.DEV ? '' : 'https://wpivis.github.io/gpt-vis';
 
   return (
     <Container>
       <Group m={5} p={5} sx={{ border: '1px solid black', borderRadius: '5px' }}>
-        <Image maw={700} src={`/gpt-expert/${imgName}`} alt="image" width="100%" />
+        <Image maw={700} src={`${imgPathPrefix}/gpt-expert/${imgName}`} alt="image" width="100%" />
         <Stack mt="xs">
           <Text>{question}</Text>
           {
